961f9ab1b8ff846e6cc9a0c72f806141b1502805
Adjei <samensah036@st.ug.edu.gh>
2025-06-27 23:02:17 +0000

    feat: implement basic HTTP server and serve static HTML page

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log("Hello, World!");
+import http, { IncomingMessage, ServerResponse } from "http";
+import path from "path";
+import fs from "fs/promises";
+
+async function requestListener(req: IncomingMessage, res: ServerResponse) {
+  const filepath = path.join(__dirname, "static/index.html");
+
+  const data = await fs.readFile(filepath);
+
+  res.writeHead(200, {
+    "content-type": "text/html",
+    "content-length": data.length,
+  });
+
+  res.write(data);
+
+  res.end();
+}
+
+http.createServer(requestListener).listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
